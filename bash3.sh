5e48975a81191ef8b53eb83828ae45cadb5954c9
karem mostafa <karem.mostafa1231@gmail.com>
2023-12-07 19:46:47 +0200

    the_scrript_for_task_3

---
+++ bash3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~
+if [ -f .bashrc ]; then 
+echo ".bashrc is found " 
+echo "export HELLO=$HOSTNAME">> .bashrc
+echo "LOCAL=$(whoami)" >> .bashrc
+gnome-terminal 
+else 
+echo ".bashrc not found "
+fi
